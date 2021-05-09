2d604867becfdb19edce3f868e42cb558265d9a4
Harvery <harvery@Harverys-MacBook-Pro.local>
2021-05-09 15:35:02 -0400

    set is loading and persons finctions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { useState , useEffect} from 'react';
 import './App.css';
+import axios from "axios";
 import Header from './components/ui/Header'
 import { useEffect, useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const  result = await axios()
+    }
+  })
   
   return (
     <div className="container">
