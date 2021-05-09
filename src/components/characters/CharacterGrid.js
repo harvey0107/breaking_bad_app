f1cc5e3de8703ded82ae8c6a9d6e4bb83ad05b46
Harvery <harvery@Harverys-MacBook-Pro.local>
2021-05-09 17:39:44 -0400

    bugs

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -1,15 +1,19 @@
 import React from 'react'
 
 const CharacterGrid = ({ items, isLoading }) => {
+    console.log(items)
     return isLoading ? (
         <h1>Loading...</h1>
     ) : (
-        <section className='cards'>
-            {items.map(item => (
+        <section className="cards">
+            {items?.map((item) => (
                 <h1>{item.name}</h1>
+                
             ))}
+            
     </section>
     )
 }
+
 
 export default CharacterGrid
