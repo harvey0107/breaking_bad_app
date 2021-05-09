e8cbfd3437747b575fbbcc4355c3ff00b06548bb
Harvery <harvery@Harverys-MacBook-Pro.local>
2021-05-09 14:37:06 -0400

    added img file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello Lin
-    </div>
-  );
+const App = () => {
+  return <div className="container"> Hello Lin</div>
 }
 
 export default App;
