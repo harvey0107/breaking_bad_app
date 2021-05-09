2fac50b21707132f24e93d5c7fbf66a1b9e1ed7d
Harvery <harvery@Harverys-MacBook-Pro.local>
2021-05-09 15:43:09 -0400

    axsios to fetch api

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState , useEffect} from 'react';
 import './App.css';
 import axios from "axios";
 import Header from './components/ui/Header'
-import { useEffect, useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -10,9 +9,11 @@ const App = () => {
 
   useEffect(() => {
     const fetchItems = async () => {
-      const  result = await axios()
+      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+      console.log(result.data)
     }
-  })
+    fetchItems()
+  }, [])
   
   return (
     <div className="container">
